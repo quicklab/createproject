094f822219990bedb266190107917005adaa3e93
quicklab <quicklab@master-0.sharedocp39.lab.rdu2.cee.redhat.com>
2019-08-07 18:41:11 -0400

    reverting back since last commit disabled both stderr and stdout

--- project_create.php
+++ project_create.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_GET["project"])) {
-shell_exec("oc new-project {$_GET['project']} 2>&1");
+shell_exec("oc new-project {$_GET['project']}");
 } else {
 echo "<h1> ERROR: Which project to create? </h1>";
 }
