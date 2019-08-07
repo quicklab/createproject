52d385d1771134e80625476ece5c0f942bad8ccd
quicklab <quicklab@master-0.sharedocp39.lab.rdu2.cee.redhat.com>
2019-08-07 18:21:20 -0400

    replacing system with shell_exec

--- project_create.php
+++ project_create.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_GET["project"])) {
-system("oc new-project {$_GET['project']}");
+shell_exec("oc new-project {$_GET['project']}");
 } else {
 echo "<h1> ERROR: Which project to create? </h1>";
 }
