f678698be476f76af8776652e82be17077b47a6e
quicklab <quicklab@master-0.sharedocp39.lab.rdu2.cee.redhat.com>
2019-08-07 09:19:51 -0400

    new-project

--- project_create.php
+++ project_create.php
@@ -1,6 +1,6 @@
 <?php
 if (is_exists($_GET["project"])) {
-system("oc create project {$_GET['project']}");
+system("oc new-project {$_GET['project']}");
 } else {
 echo "<h1> ERROR: Which project to create? </h1>";
 }
